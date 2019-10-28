00ef00b1c9a5dd507faf2c6be5b3318ae4a86585
Eros Innocenti <eros@newtechweb.it>
2019-10-28 07:26:36 +0100

    Integrata darknet e yolov3

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 import { env } from './environment/environment';
 const WebSocket = require('ws');
+const fs = require('fs');
+const cv = require('opencv4nodejs');
+const { Darknet } = require('darknet'); 
 
 function noop() {}
 
@@ -10,10 +13,29 @@ wss.on('connection', (ws, req) => {
 	ws.alive = true;
 
 	console.log('New client connected (' + req.connection.remoteAddress + ')');
+	
+	console.log('Streaming started');
 
+	// Init
+	let darknet = new Darknet({
+		weights: './config/yolov3-tiny.weights',
+		config: './config/yolov3-tiny.cfg',
+		names: [ 'person' ]
+	});
+
+	const cap = new cv.VideoCapture('./data/leeds.mp4');
+	
+	let frame;
+	do {
+		frame = cap.read().cvtColor(cv.COLOR_BGR2RGB);
+		ws.send(darknet.detect(frame));
+		console.log(darknet.detect(frame));
+	} while(!frame.empty && ws.alive);
+	
 	ws.on('message', function incoming(message) {});
 
 	ws.on('close', () => {
+		ws.alive = false;
 		console.log('Client disconnected');
 	});
 });
